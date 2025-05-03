2a97128caaf3cbf83bf7cd6354c06913e8f4039e
silentShulk <marcostrada258@gmail.com>
2025-05-03 19:08:26 +0200

    Started working on password ,manager, but will probably change everything

--- PasswordManager/src/main.rs
+++ PasswordManager/src/main.rs
@@ -1,3 +1,20 @@
+use std::path;
+mod functions;
+use clap::Parser;
+use functions::PswrdEntry;
+
 fn main() {
-    println!("Hello, world!");
+    let args = functions::Arguments::parse();   
+
+    const STORAGE_FILE: &str= "~/.passwords";
+    let storage_file_path = path::PathBuf::from(STORAGE_FILE);
+
+    let entry = PswrdEntry {
+        password: args.password,
+        service: args.service,
+    };
+    let serialized_entry = serde_json::to_string(&entry).unwrap();
+
+    let mut stored_data = functions::retrieve_passwords_array(storage_file_path).unwrap();
+    stored_data.push(serialized_entry);
 }
