bd07b8175fff55b9066887ab0973a553567e90be
silentShulk <marcostrada258@gmail.com>
2025-05-05 19:22:32 +0200

    Tried implementing basic functionalioty, will pobably drop project because of lacking knowledge

--- PswrdMngrREPL/src/functions.rs
+++ PswrdMngrREPL/src/functions.rs
@@ -1,3 +1,11 @@
+use std::collections::HashMap;
+use aes_gcm::{Aes256Gcm, Key, Nonce};
+use aes_gcm::aead::{Aead, KeyInit};
+use rand::Rng;
+use bincode;
+
+const SECRET_FILE_PATH: &str = "$HOME/.local/share/<your-app>/passwords.json";
+
 pub struct Command {
     pub functionality: String,
     pub args: Vec<String>,
@@ -16,16 +24,25 @@ pub fn string_to_command(input: &str) -> Command {
 
 pub fn execute_command(command: &Command) {
     match command.functionality.as_str() {
-        "add-password" => add_password(),
-        "get-password" => get_password(),
+        "add-password" => add_password(&command.args),
+        "get-password" => get_password(&command.args),
         _ => println!("Command not found: {}", command.functionality)
     }
 }
 
-fn add_password() {
+fn add_password(args: Vec<String>) {
+    let password = &args[0];
+    let service = &args[1];
+
+    let password_store: HashMap<String, String> = vec![(service, password)].iter().collect();
+}
+
+fn get_password(args: &Vec<String>) {
 
 }
 
-fn get_password() {
+fn encrypt_and_save(filepath: &str, key_bytes: &[u8; 32]) {
+    let serialized = bincode::encode_to_vec(store, bincode::config::Standard)
+    .expect("Failed to serialize");
 
 }
